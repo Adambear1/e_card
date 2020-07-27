98f705fc913fef41f4cb3a051839f03a75a76df9
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-07-27 10:50:10 -0700

    react is so much fun

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
 
-function Index() {
+export default class Index extends Component {
+  state = {
+    isLoggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: "",
+  };
+  componentClicked = () => console.log("clicked")
+  responseFacebook = response => console.log(response)
+  render(){
+    let googleContent
+    if(state.isLoggedIn){
+
+    } else {
+      googleContent =  (<GoogleLogin appId="" autoLoad={true} autoLoad={true} fields="name,email,picture" onClick={this.componentClicked}
+      callback={this.responseGoogle}/>)
+    }
+  }
   return (
     <Container fluid>
       <Row>
-        <Col size="md-6"></Col>
+        <Col size="md-6">
+          <a href="auth/google">
+            <i class="fa fa-google" aria-hidden="true"></i>
+          </a>
+        </Col>
       </Row>
     </Container>
   );
